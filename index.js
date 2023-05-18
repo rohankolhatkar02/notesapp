59e0ef6f2527c04307074b25b44d3434ae11f41e
rohan <rohankolhatkar02@gmail.com>
2023-05-18 21:50:53 +0530

    commit3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var express = require ("express")
-var bodyParser = require ("body-parser")
-var mongoose = require ("mongoose")
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
 
 const methodOverride = require('method-override');
 
@@ -9,9 +9,9 @@ const bcrypt = require('bcrypt');
 
 const app = express()
 const MongoClient = require('mongodb').MongoClient;
-const url='mongodb://127.0.0.1:27017/miniproject?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.2';
+const url = 'mongodb://127.0.0.1:27017/miniproject?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.2';
 
-const MyModel=require('./schema.js');
+const MyModel = require('./schema.js');
 
 const dbName = 'miniproject';
 
@@ -28,26 +28,26 @@ app.use(bodyParser.json())
 app.use(express.json());
 app.use(express.static('public'))
 app.use(express.urlencoded({
-    extended:true
+  extended: true
 }))
 app.use(bodyParser.urlencoded({
-    extended:true
+  extended: true
 }))
 
 app.use(methodOverride('_method'));
 
-mongoose.set('strictQuery',true);
+mongoose.set('strictQuery', true);
 
-mongoose.connect('mongodb://127.0.0.1:27017/miniproject?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.2',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect('mongodb://127.0.0.1:27017/miniproject?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.2', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 var db = mongoose.connection;
 
 
-db.on('error',()=>console.log("Error in Connecting to Database"))
-db.once('open',()=>console.log("Connected to Database"))
+db.on('error', () => console.log("Error in Connecting to Database"))
+db.once('open', () => console.log("Connected to Database"))
 
 
 app.use(express.static("public"));
@@ -56,75 +56,75 @@ app.use(methodOverride('_method'));
 
 
 //login page route
-app.post("/login",(req,res)=>{
-    var name = req.body.name;
-    var password = req.body.password;
+app.post("/login", (req, res) => {
+  var name = req.body.name;
+  var password = req.body.password;
 
-    // Find a user with the provided email in the database
-    db.collection('users').findOne({name: name}, (err, user) => {
+  // Find a user with the provided email in the database
+  db.collection('users').findOne({ name: name }, (err, user) => {
+    if (err) {
+      throw err;
+    }
+
+    if (!user) {
+      // If there is no user with the provided email, return an error message
+      return res.status(401).send("INVALID EMAIL OR PASSWORD");
+    }
+
+    // Compare the provided password with the hashed password in the database
+    bcrypt.compare(password, user.password, (err, result) => {
       if (err) {
         throw err;
       }
 
-      if (!user) {
-        // If there is no user with the provided email, return an error message
+      if (result === true) {
+        // If the password matches, redirect the user to the home page
+        return res.redirect('devices');
+      } else {
+        // If the password doesn't match, return an error message
         return res.status(401).send("INVALID EMAIL OR PASSWORD");
       }
-
-      // Compare the provided password with the hashed password in the database
-      bcrypt.compare(password, user.password, (err, result) => {
-        if (err) {
-          throw err;
-        }
-
-        if (result === true) {
-          // If the password matches, redirect the user to the home page
-          return res.redirect('devices');
-        } else {
-          // If the password doesn't match, return an error message
-          return res.status(401).send("INVALID EMAIL OR PASSWORD");
-        }
-      });
     });
+  });
 });
 
- //FOR USER REGISTRATION
+//FOR USER REGISTRATION
 
- app.post("/register",(req,res)=>{
-    var name = req.body.name;
-    var password = req.body.password;
+app.post("/register", (req, res) => {
+  var name = req.body.name;
+  var password = req.body.password;
 
-    // Hash the password with bcrypt
-    bcrypt.hash(password, 10, (err, hash) => {
+  // Hash the password with bcrypt
+  bcrypt.hash(password, 10, (err, hash) => {
+    if (err) {
+      throw err;
+    }
+
+    var data = {
+      "name": name,
+      "password": hash,
+    }
+
+    // Insert the user data into the database
+    db.collection('users').insertOne(data, (err, collection) => {
       if (err) {
         throw err;
       }
-
-      var data = {
-        "name":name,
-        "password": hash,
-      }
-
-      // Insert the user data into the database
-      db.collection('users').insertOne(data,(err,collection)=>{
-        if(err){
-            throw err;
-        }
-        console.log("Record inserted successfully");
-      });
-
-      // Serve the login page
-      res.sendFile(path.join(__dirname + '/public/index.html'));
+      console.log("Record inserted successfully");
     });
+
+    // Serve the login page
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+  });
 });
 
 //adding notes to Database
 app.post('/notes', (req, res) => {
   const { title, text } = req.body;
-  const note = { 
-    title, 
+  const note = {
+    title,
     text,
-    created_at:  new Date() // get date and time of instance
+    created_at: new Date() // get date and time of instance
   };
   db.collection('notes').insertOne(note, (err, result) => {
     if (err) {
@@ -190,13 +190,14 @@ app.get('/notes/:id/update', async (req, res) => {
 })
 
 // Update a note
+// Update a note
 app.post('/notes/:id', async (req, res) => {
   try {
     const { title, content } = req.body
-    const note = await MyModel.findById(req.params.id)
-    note.title = title
-    note.content = content
-    await note.save()
+    const device = await MyModel.findById(req.params.id)
+    device.title = title
+    device.text = content // change this from device.content to device.text
+    await device.save()
     res.redirect('/notes')
   } catch (err) {
     console.log(err)
@@ -207,22 +208,28 @@ app.post('/notes/:id', async (req, res) => {
 app.put('/notes/:id', async (req, res) => {
   const { title, content } = req.body;
   const { id } = req.params;
-  
-  try {
-    // find the note in the database with the given id
-    const note = await MyModel.findById(id);
 
-    // update the note with the new title and content
-    note.title = title;
-    note.content = content;
-    await note.save();
-    
+  try {
+    const device = await MyModel.findById(id);
+    if (!device) {
+      return res.status(404).send('Note not found');
+    }
+
+    if (!content) {
+      return res.status(400).send('Text field is required');
+    }
+
+    device.title = title;
+    device.text = content; // change this from device.content to device.text
+    await device.save();
+
     res.redirect('/devices');
   } catch (err) {
     console.log(err);
     res.redirect('/');
   }
 });
+
 
 
 
@@ -261,15 +268,14 @@ app.get('/update/:id', (req, res) => {
 
 
 app.listen(4001, '0.0.0.0', () => {
-    console.log('Server is listening on port 4001');
-  });
-  
-  app.get("/",(req,res)=>{
-     res.set({
-          "Allow-access-Allow-Origin":'*'
-      })
-      return res.redirect('login.html')
-  }).listen(4000);
-  
-  console.log("listening on PORT 4000");
-  
+  console.log('Server is listening on port 4001');
+});
+
+app.get("/", (req, res) => {
+  res.set({
+    "Allow-access-Allow-Origin": '*'
+  })
+  return res.redirect('login.html')
+}).listen(4000);
+
+console.log("listening on PORT 4000");
